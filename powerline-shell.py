8eff0d2b9826a5f69b6b243a5f4d0e04d3bc8f5d
Chris Scharf <scharfie@gmail.com>
2013-01-31 08:21:52 -0500

    removed root indicator segment

--- powerline-shell.py
+++ powerline-shell.py
@@ -333,7 +333,7 @@ if __name__ == '__main__':
     #p.append(Segment(p, ' \\h ', 250, 238))
     add_cwd_segment(p, cwd, 5, args.cwd_only)
     add_repo_segment(p, cwd)
-    add_root_indicator(p, args.prev_error)
+    # add_root_indicator(p, args.prev_error)
     sys.stdout.write(p.draw())
 
 # vim: set expandtab:
